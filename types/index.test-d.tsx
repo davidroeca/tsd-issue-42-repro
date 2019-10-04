79a528340b71a9801501b963653109dd08f9d694
davidroeca <david.roeca@gmail.com>
2019-10-04 19:35:26 -0400

    prettier the file

--- types/index.test-d.tsx
+++ types/index.test-d.tsx
@@ -3,48 +3,19 @@ import { expectType, expectError } from 'tsd'
 import { MyComponent } from '.'
 
 // expected interface
-expectType<JSX.Element>(
-  <MyComponent
-    prop1="Hello"
-    prop2={123}
-  />
-)
+expectType<JSX.Element>(<MyComponent prop1="Hello" prop2={123} />)
 
 // This one's fine
-expectError(
-  <MyComponent
-    foo="bar"
-  />
-)
+expectError(<MyComponent foo="bar" />)
 
 // This one's also fine
-expectError(
-  <MyComponent
-    prop1={1}
-    prop2={2}
-  />
-)
+expectError(<MyComponent prop1={1} prop2={2} />)
 
 // This one's also fine
-expectError(
-  <MyComponent
-    prop1="hey"
-    prop2={1234}
-    iShouldNotBe="here"
-  />
-)
+expectError(<MyComponent prop1="hey" prop2={1234} iShouldNotBe="here" />)
 
 // This one's broken
-expectError(
-  <MyComponent
-    prop1="hello"
-  />
-)
-
+expectError(<MyComponent prop1="hello" />)
 
 // This one too
-expectError(
-  <MyComponent
-    prop2={2}
-  />
-)
+expectError(<MyComponent prop2={2} />)
